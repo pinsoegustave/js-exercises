5c6de83e3363fa7f42cde0d8600cafcf561355f0
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-01 19:56:18 +0200

    Not serious

---
+++ js-dom/formValues.js
@@ -0,0 +1,12 @@
+function getFormvalue() {
+  var x=document.getElementById("form1");
+  console.log(x);
+  for (var i=0;i<x.length;i++)
+  {
+   if (x.elements[i].value =='Submit')
+    {  
+      console.log(x.elements[i].value);
+     }  
+   }
+
+}

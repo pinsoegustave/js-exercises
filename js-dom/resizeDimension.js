e0d3d0c4b81efca179574cfd4a61fa626eba6368
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-02 15:02:51 +0200

    JS DOM: Get window size

---
+++ js-dom/resizeDimension.js
@@ -0,0 +1,9 @@
+
+
+function getSize() {
+    var w = document.documentElement.clientWidth;
+
+    var h = document.documentElement.clientHeight;
+
+    document.getElementById('wh').innerHTML = "<h1>Width: "+ w + "Height: "+ h +"</h1>"
+}
